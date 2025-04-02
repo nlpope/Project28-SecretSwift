655103142403b64faca16131724d961e13c74985
Noah Pope <npope@tutanota.com>
2025-04-02 10:25:33 -0500

    Initial Commit

---
+++ Project28-SecretSwift/ViewController.swift
@@ -0,0 +1,16 @@
+//  File: ViewController.swift
+//  Project: Project28-SecretSwift
+//  Created by: Noah Pope on 4/2/25.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project28-SecretSwiftTests/Project28_SecretSwiftTests.swift
@@ -0,0 +1,14 @@
+//  File: Project28_SecretSwiftTests.swift
+//  Project: Project28-SecretSwiftTests
+//  Created by: Noah Pope on 4/2/25.
+
+import Testing
+@testable import Project28_SecretSwift
+
+struct Project28_SecretSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
