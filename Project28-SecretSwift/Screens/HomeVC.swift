fa10eea8adf9f5b2268ca4e475b73f2c17199e5a
Noah Pope <npope@tutanota.com>
2025-04-02 13:00:47 -0500

    setting up keyboard notifications

--- Project28-SecretSwift/Screens/HomeVC.swift
+++ Project28-SecretSwift/Screens/HomeVC.swift
@@ -11,5 +11,21 @@ class HomeVC: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        setUpKeyboardNotifications()
+    }
+    
+    
+    func setUpKeyboardNotifications()
+    {
+        let notificationCenter  = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    
+    @objc func adjustForKeyboard(notification: Notification)
+    {
+        guard let keyboardValue     = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+        else { return }
     }
 }
