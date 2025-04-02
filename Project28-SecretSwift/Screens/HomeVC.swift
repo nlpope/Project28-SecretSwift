8b5d277f5c578d42e8443ba23a5380067f264088
Noah Pope <npope@tutanota.com>
2025-04-02 10:38:27 -0500

    Initial commit.

---
+++ Project28-SecretSwift/Screens/HomeVC.swift
@@ -0,0 +1,15 @@
+//  File: HomeVC.swift
+//  Project: Project28-SecretSwift
+//  Created by: Noah Pope on 4/2/25.
+
+import UIKit
+
+class HomeVC: UIViewController
+{
+    @IBOutlet var secret: UITextView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
