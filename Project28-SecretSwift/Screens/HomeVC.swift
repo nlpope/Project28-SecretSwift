1fe0ea3b02a9ebd68d71b91a1662d8ea2c2c4f3e
Noah Pope <npope@tutanota.com>
2025-04-03 15:14:33 -0500

    incorporating MIT's KeychainWrapper & KeychainItemAccessibility source files + writing logic to authenticateTapped method.

--- Project28-SecretSwift/Screens/HomeVC.swift
+++ Project28-SecretSwift/Screens/HomeVC.swift
@@ -3,6 +3,7 @@
 //  Created by: Noah Pope on 4/2/25.
 
 import UIKit
+import LocalAuthentication
 
 class HomeVC: UIViewController
 {
@@ -11,21 +12,114 @@ class HomeVC: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        setUpNavigation()
+        setUpApplicationNotifications()
         setUpKeyboardNotifications()
     }
     
     
+    func setUpNavigation()
+    {
+        title = SecretKeys.secretTitle
+    }
+    
+    
+    @IBAction func authenticateTapped(_ sender: Any)
+    {
+        let ctx = LAContext()
+        var error: NSError?
+        
+        /** if you can evaluate the policy as true (here's a pointer to my error var btw, you viejo Obj-C remnant) ...
+         then evalueate the policy with the OG NSError you set up who's value should now be filled via mem address
+         after '.canEvalPolicy' returned its Bool*/
+        
+        if ctx.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            evaluatePolicy(inContext: ctx, error: error)
+        } else {
+            /** handle error */
+        }
+    }
+    
+    
+    func evaluatePolicy(inContext ctx: LAContext, error: NSError?)
+    {
+        let reason = "Identify yourself!"
+        
+        ctx.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
+            DispatchQueue.main.async {
+                if success { self?.unlockSecretMessage() }
+                else { /** handle error */ }
+            }
+        }
+    }
+    
+    
+    func unlockSecretMessage()
+    {
+        secret.isHidden = false
+        title           = "Secret stuff!"
+        
+        secret.text     = KeychainWrapper.standard.string(forKey: SecretKeys.secretMessage) ?? ""
+    }
+    
+    
+    @objc func saveSecretMessage()
+    {
+        guard secret.isHidden == false else { return }
+        
+        KeychainWrapper.standard.set(secret.text, forKey: SecretKeys.secretMessage)
+        secret.resignFirstResponder()
+        secret.isHidden = true
+        title           = SecretKeys.secretTitle
+    }
+    
+    //-------------------------------------//
+    // MARK: APPLICATION NOTIFICATION METHODS
+    
+    func setUpApplicationNotifications()
+    {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(saveSecretMessage),
+                                       name: UIApplication.willResignActiveNotification,
+                                       object: nil)
+    }
+    
+    //-------------------------------------//
+    // MARK: KEYBOARD NOTIFICATION METHODS
+    
     func setUpKeyboardNotifications()
     {
         let notificationCenter  = NotificationCenter.default
-        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        notificationCenter.addObserver(self,
+                                       selector: #selector(adjustForKeyboard),
+                                       name: UIResponder.keyboardWillHideNotification,
+                                       object: nil)
+        notificationCenter.addObserver(self,
+                                       selector: #selector(adjustForKeyboard),
+                                       name: UIResponder.keyboardWillChangeFrameNotification,
+                                       object: nil)
     }
     
     
     @objc func adjustForKeyboard(notification: Notification)
     {
-        guard let keyboardValue     = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+        guard let keyboardValue         = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
         else { return }
+        let keyboardScreenEndFrame      = keyboardValue.cgRectValue
+        let keyboardViewEndFrame        = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification { secret.contentInset = .zero }
+        else {
+            secret.contentInset = UIEdgeInsets(top: 0,
+                                               left: 0,
+                                               bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom,
+                                               right: 0)
+        }
+        
+        secret.scrollIndicatorInsets    = secret.contentInset
+        
+        let selectedRange               = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
     }
 }
---
+++ Project28-SecretSwift/Utilities/Constants+Utils.swift
@@ -0,0 +1,11 @@
+//  File: Constants+Utils.swift
+//  Project: Project28-SecretSwift
+//  Created by: Noah Pope on 4/3/25.
+
+import Foundation
+
+enum SecretKeys
+{
+    static let secretTitle      = "Nothing to see here"
+    static let secretMessage    = "SecretMessage"
+}
